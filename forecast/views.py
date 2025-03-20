192967a216e14dc70d8264dcea4f5bce86c9aa21
Khumoyun <humoyunortiqov05@gmail.com>
2025-03-20 12:36:06 +0500

    forecast, location, weatherData larga viewslar yozildi

--- forecast/views.py
+++ forecast/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import serializers
+from .models import Forecast
 
-# Create your views here.
+class ForecastMixin:
+    queryset = Forecast.objects.all()
+    serializer_class = serializers.ForecastSerializer
+
+class ForecastApiView(ForecastMixin, generics.ListCreateAPIView):
+    pass
+
+class ForecastDetailApiView(ForecastMixin, generics.RetrieveUpdateDestroyAPIView):
+    pass
+
+class ForecastLocationApiView(generics.ListAPIView):
+    queryset = Forecast.objects.all()
+    serializer_class = serializers.ForecastLocationSerializer
+
+    def get_queryset(self):
+        return super().get_queryset().filter(location__id=self.kwargs['pk'])
--- location/views.py
+++ location/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import LocationSerializer
+from .models import Location
 
-# Create your views here.
+class LocationMixin:
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class LocationApiView(LocationMixin, generics.ListCreateAPIView):
+    pass
+
+class LocationDetailApiView(LocationMixin, generics.RetrieveUpdateDestroyAPIView):
+    pass
